7766c11ca1f949997d205cf4d5bd754fc1585346
Satyadeo <satyadeo@outlook.in>
2022-05-15 23:44:29 +0530

    My First Java Code

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !! This is my first Java Code");
+        System.out.println("This is to test IntelliJ and git integration");
     }
 }
