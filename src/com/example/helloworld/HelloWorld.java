0abc971c5e1c632d87f3488b85b38054f4778afe
Satyadeo <satyadeo@outlook.in>
2022-05-15 23:40:41 +0530

    My First Java Code

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !! This is my first Java Code");
+    }
+}
